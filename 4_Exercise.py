6851f17100c1800a99021080d64b9e09de8414ff
YOGIRAJ3577 <104525081+YOGIRAJ3577@users.noreply.github.com>
2026-08-05 01:29:26 +0530

    Create 4_Exercise.py
    First Exercise solved

---
+++ 4_Exercise.py
@@ -0,0 +1,7 @@
+first_name = input("Enter First name : ")
+last_name = input("Enter Last name : ")
+
+age = 53
+height = 1.89
+
+print (first_name + " "+last_name + " is a secratly a super hero and his age is "+ str(age)+" and his height is "+ str(height))
