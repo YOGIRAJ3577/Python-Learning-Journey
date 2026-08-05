b300dd63eb131da66c5b2cd901480ec1bd65805a
YOGIRAJ3577 <104525081+YOGIRAJ3577@users.noreply.github.com>
2026-08-06 00:46:21 +0530

    List Tuple Set  Dict

---
+++ 13_Tuple.py
@@ -0,0 +1,4 @@
+#Tuple = Immutable list
+
+Marks = (96, 94,85,90,99)
+print(Marks, type(Marks)) # (96, 94, 85, 90, 99) <class 'tuple'>
---
+++ 14_Exericise_ListTupleSet.py
@@ -0,0 +1,9 @@
+Roll = [101, 102, 103, 104, 105,102,103,101,104,105]
+
+RollSet = set(Roll)
+print(RollSet) # {101, 102, 103, 104, 105}
+
+EMP = [("John", 101,5000), ("Smith", 102,6000), ("David", 103,7000), ("John", 105,5000), ("Smith", 102,6000)]
+EMPSet = set(EMP)
+print(EMP)
+print(EMPSet) # {('John', 101, 5000), ('Smith', 102, 6000), ('David', 103, 7000)}
