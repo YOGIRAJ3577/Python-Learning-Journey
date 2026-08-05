fcd95f3cdff0345ff7fd739b69eba65c1fb6b8f9
YOGIRAJ3577 <104525081+YOGIRAJ3577@users.noreply.github.com>
2026-08-05 17:09:53 +0530

    Create 7_Exerice2.py

---
+++ 7_Exerice2.py
@@ -0,0 +1,10 @@
+ValueA = float(input("Enter A price of first product : "))
+Valueb = float(input("Enter A price of 2nd product : "))
+Valuec = float(input("Enter A price of 3rd product : "))
+
+TotalValue = ValueA + Valueb + Valuec
+print("The total price of three products is : ", TotalValue)
+print("The average price of the three products is : ", TotalValue/3)
+
+superHero = input("enter a superHero name : ")
+print("S" in superHero)
