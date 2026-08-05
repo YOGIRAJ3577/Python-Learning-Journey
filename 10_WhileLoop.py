b0d226774789e642c27113cc2f218c8e51eeacbd
YOGIRAJ3577 <104525081+YOGIRAJ3577@users.noreply.github.com>
2026-08-05 23:28:56 +0530

    Loops

---
+++ 10_WhileLoop.py
@@ -0,0 +1,10 @@
+counter=1
+
+while counter<=10:
+    print(counter)
+    counter += 1
+
+i=1
+while i<=5:
+    print(i * " * ")
+    i += 1
---
+++ 9_Range_ForrLoop.py
@@ -0,0 +1,13 @@
+num = range(5)
+
+print(num)  # Output: range(0, 5)
+
+range(1,6)
+print(range(1, 6))  # Output: range(1, 6)
+
+for i in num:
+    print(i )  # Output: 0, 1, 2, 3, 4
+
+print("End of first for loop")
+for i in range(1,6):
+    print(i )  
